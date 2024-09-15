e182f232b83ddad367a78ab151b23f720183bd4b
Joseph Crump <josephcrump79@yahoo.com>
2024-09-15 14:19:44 -0500

    phase-1-building-simple-liker

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+const errorModal = document.getElementById('modal')
+errorModal.hidden = true
+const hearts = document.getElementsByClassName('like-glyph')
+for (const element of hearts) {
+  element.addEventListener('click', () => {
+    if (element.classList.contains ('activated-heart')){
+      //if the heart is not empty 
+      element.classList.remove('activated-heart')
+      element.textContent = EMPTY_HEART
+    }
+    else {
+      mimicServerCall()
+      .then(()=>{
+        element.classList.add('activated-heart')
+      element.textContent = FULL_HEART
+      }) 
+      .catch((error) => {
+        errorModal.hidden = false
+        errorModal.textContent = error
+        setTimeout(() => {
+          errorModal.hidden = true
+        },3000)
+      })
+    }
+  });
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
